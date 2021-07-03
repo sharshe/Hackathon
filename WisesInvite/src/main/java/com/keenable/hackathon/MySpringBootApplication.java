50a260214fd365645eccaa012680554c0f2e5dd2
Shardul Shekhar <shardul.shekhar@fosteringlinux.com>
2021-07-03 10:33:21 +0530

    Update MySpringBootApplication.java

--- WisesInvite/src/main/java/com/keenable/hackathon/MySpringBootApplication.java
+++ WisesInvite/src/main/java/com/keenable/hackathon/MySpringBootApplication.java
@@ -1,18 +1,37 @@
 package com.keenable.hackathon;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
+import org.apache.camel.Exchange;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.ArrayList;
 
-@SpringBootApplication
-@ImportResource({"classpath:spring/camel-context.xml"})
-public class MySpringBootApplication {
+class updateField{
 
-    /**
-     * A main method to start this application.
-     */
-    public static void main(String[] args) {
-        SpringApplication.run(MySpringBootApplication.class, args);
+    public void showData(Exchange exchange){
+
+        Map <String , Object > map = new HashMap<String , Object > (exchange.getIn().getBody(Map.class));
+
+        System.out.println(" Bean Method : "+map);
+        System.out.println(" Bean Method : "+map.keySet() );
+        String SecoundKey = map.keySet().toArray()[1].toString();
+        String secoundKeyValue = map.get(SecoundKey).toString();
+        System.out.println(" Bean key : "+SecoundKey +" Value "+secoundKeyValue );
+        exchange.getIn().setHeader("SecoundKey",SecoundKey);
+
+    }
+
+    public void getKey(Exchange exchange){
+
+        Map <String , Object > map = new HashMap<String , Object > (exchange.getIn().getBody(Map.class));
+
+        System.out.println(" Bean Method : "+map);
+        System.out.println(" Bean Method : "+map.keySet() );
+        String Key = map.keySet().toArray()[0].toString();
+        String KeyValue = map.get(Key).toString();
+        System.out.println(" Bean key : "+Key +" Value "+KeyValue );
+        exchange.getIn().setHeader("key",Key);
+
     }
 
 }
